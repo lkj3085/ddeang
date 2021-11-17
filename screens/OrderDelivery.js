ad58f8409ab7da060f4352a1980082a730a9649d
lkj3085 <lkj3085@gmail.com>
2021-11-17 19:40:35 +0900

    ddeang

---
+++ screens/OrderDelivery.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import {COLORS, FONTS, icons, SIZES, GOOGLE_API_KEY} from '../constants';
+import MapViewDirections from 'react-native-maps-directions';
+
+const OrderDelivery = ({route, navigation}) => {
+  const mapView = React.useRef();
+
+  const [restaurant, setRestaurant] = React.useState(null);
+  const [streetName, setSteetName] = React.useState('');
+  const [fromLocation, setFromLocation] = React.useState(null);
+  const [toLocation, setToLocation] = React.useState(null);
+  const [region, setRegion] = React.useState(null);
+
+  const [duration, setDuration] = React.useState(0);
+  const [isReady, setIsReady] = React.useState(false);
+  const [angle, setAngle] = React.useState(0);
+
+  React.useEffect(() => {
+    let {restaurant, currentLocation} = route.params;
+
+    let fromLoc = currentLocation.gps;
+    let toLoc = restaurant.location;
+    let street = currentLocation.streetName;
+
+    let mapRegion = {
+      latitude: (fromLoc.latitude + toLoc.latitude) / 2,
+      longitude: (fromLoc.longitude + toLoc.longitude) / 2,
+      latitudeDelta: Math.abs(fromLoc.latitude - toLoc.latitude) * 2,
+      longitudeDelta: Math.abs(fromLoc.longitude - toLoc.longitude) * 2,
+    };
+
+    setRestaurant(restaurant);
+    setSteetName(street);
+    setFromLocation(fromLoc);
+    setToLocation(toLoc);
+    setRegion(mapRegion);
+  }, []);
+
+  function calculateAngle(coordinates) {
+    let startLat = coordinates[0]['latitude'];
+    let startLng = coordinates[0]['longitude'];
+    let endLat = coordinates[1]['latitude'];
+    let endLng = coordinates[1]['longitude'];
+    let dx = endLat - startLat;
+    let dy = endLng - startLng;
+
+    return (Math.atan2(dy, dx) * 180) / Math.PI;
+  }
+
+  function renderMap() {
+    const destinationMarker = () => (
+      <Marker coordinate={toLocation}>
+        <View
+          style={{
+            height: 40,
+            width: 40,
+            borderRadius: 20,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: COLORS.white,
+          }}>
+          <View
+            style={{
+              height: 30,
+              width: 30,
+              borderRadius: 15,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: COLORS.primary,
+            }}>
+            <Image
+              source={icons.pin}
+              style={{width: 25, height: 25, tintColor: COLORS.white}}
+            />
+          </View>
+        </View>
+      </Marker>
+    );
+
+    const carIcon = () => (
+      <Marker
+        coordinate={fromLocation}
+        anchor={{x: 0.5, y: 0.5}}
+        flat={true}
+        rotation={angle}>
+        <Image source={icons.car} style={{width: 40, height: 40}} />
+      </Marker>
+    );
+
+    return (
+      <View style={{flex: 1}}>
+        <MapView
+          ref={mapView}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={region}
+          style={{flex: 1}}>
+          <MapViewDirections
+            origin={fromLocation}
+            destination={toLocation}
+            apikey={GOOGLE_API_KEY}
+            strokeWidth={5}
+            strokeColor={COLORS.primary}
+            optimizeWaypoints={true}
+            onReady={result => {
+              setDuration(result.duration);
+
+              if (!isReady) {
+                mapView.current.fitToCoordinates(result.coordinates, {
+                  edgePadding: {
+                    right: SIZES.width / 20,
+                    bottom: SIZES.height / 4,
+                    left: SIZES.width / 20,
+                    top: SIZES.height / 8,
+                  },
+                });
+                let nextLoc = {
+                  latitude: result.coordinates[0]['latitude'],
+                  longitude: result.coordinates[0]['longitude'],
+                };
+
+                if (result.coordinates.length >= 2) {
+                  let angle = calculateAngle(result.coordinates);
+                  setAngle(angle);
+                }
+
+                setFromLocation(nextLoc);
+                setIsReady(true);
+              }
+            }}
+          />
+          {destinationMarker()}
+          {carIcon()}
+        </MapView>
+      </View>
+    );
+  }
+
+  return <View style={{flex: 1}}>{renderMap()}</View>;
+};
+
+export default OrderDelivery;
